c815a06754a3c48f6ded41f88ee7aa6ac45bf9eb
Bunyod16 <bunyodshams@gmail.com>
2022-10-23 03:47:26 +0800

    optimised wallet creation

--- sui-create-wallet.py
+++ sui-create-wallet.py
@@ -12,113 +12,111 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
 password = '12Testing34'
+total_amount = 0
 
+def setup_sui_wallet(amount):
 
-def setup_sui_wallet():
+    global total_amount
     try:
-        # Because I use M1 Mac it has error
-        # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
         chrome_options = Options()
-        chrome_options.add_extension('./src/Sui-Wallet.crx')
+        chrome_options.add_extension('./src/suiet-sui-wallet.crx')
         driver = webdriver.Chrome(service=Service('./src/chromedriver'), options=chrome_options)
         driver.maximize_window()
-        wait = WebDriverWait(driver, 5)
-        parent = driver.window_handles[0]
+        wait = WebDriverWait(driver, 20)
+        driver.get('chrome-extension://khpkpbbcccdmmclmpigdgddabeilkdpd/index.html')
 
-        def switch_tab():
-            windows = driver.window_handles
-            for w in windows:
-                if w != parent:
-                    driver.switch_to.window(w)
-
-        driver.get('https://www.google.com')
-
-        switch_tab()
-
-        sui_wallet_get_started_btn = wait.until(
+        sui_wallet_create = wait.until(
             ec.visibility_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/div/div/div[2]/a')))
-        sui_wallet_get_started_btn.click()
+                (By.XPATH, '/html/body/div/div/div[1]/section/button[1]')))
+        sui_wallet_create.click()
 
-        sui_wallet_create_new_wallet_btn = wait.until(
+        wallet_password_field = wait.until(
             ec.visibility_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/div[1]/a')))
-        sui_wallet_create_new_wallet_btn.click()
+                (By.XPATH, '/html/body/div/div/div[1]/div/section/form/div[1]/div/div/input')))
+        wallet_password_field.click()
+        wallet_password_field.send_keys(password)
 
-        sui_wallet_create_password_input = wait.until(
+        wallet_password_field = wait.until(
             ec.visibility_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/form/div/fieldset/label[1]/input')))
-        sui_wallet_create_password_input.send_keys(password)
-
-        sui_wallet_confirm_password_input = wait.until(
+                (By.XPATH, '/html/body/div/div/div[1]/div/section/form/div[2]/div/div/input')))
+        wallet_password_field.click()
+        wallet_password_field.send_keys(password)
+        next_step = wait.until(
             ec.visibility_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/form/div/fieldset/label[2]/input')))
-        sui_wallet_confirm_password_input.send_keys(password)
-
-        sui_wallet_create_password_check_box = wait.until(
+                (By.XPATH, '/html/body/div/div/div[1]/div/section/form/button')))
+        next_step.click()
+        
+        pass_phrase = wait.until(
             ec.visibility_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/form/div/fieldset/label[3]/span[1]')))
-        sui_wallet_create_password_check_box.click()
-
-        sui_wallet_create_wallet_btn = wait.until(
+                (By.XPATH, '/html/body/div/div/div[1]/div/section/div/div[1]')))
+                
+        seed_phrase = pass_phrase.text
+        seed_phrase = seed_phrase.split('\n')
+        clean_phrase = []
+        for word in seed_phrase:
+            word = str(word)
+            if not word.isnumeric():
+                clean_phrase.append(word)
+        with open(f'seeds/{random.randint(0, 999999999)}', 'w+') as f:
+            f.write(str(clean_phrase))
+            
+        done = wait.until(
             ec.visibility_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/form/button')))
-        sui_wallet_create_wallet_btn.click()
-
-        time.sleep(2)
-
-        sui_wallet_seed_phrase = wait.until(
+                (By.XPATH, '/html/body/div/div/div[1]/div/section/button')))
+        done.click()
+        view_wallets = wait.until(
             ec.visibility_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/div[2]')))
-
-        with open(f'wallets/wallet-{int(random.random() * 10 ** 16)}', 'w') as wallet_file:
-            wallet_file.write(sui_wallet_seed_phrase.text.split('\nCOPY')[0])
-
-        sui_wallet_open_sui_wallet_btn = wait.until(
-            ec.visibility_of_element_located(
-                (By.XPATH, '/html/body/div/div/div/button')))
-        sui_wallet_open_sui_wallet_btn.click()
-
-        return 1
+                (By.XPATH, '/html/body/div/div/div[1]/div[1]/div[2]/span')))
+        view_wallets.click()
+            
+        for i in range(amount):
+            start_time = time.time()
+            creat_wallet = wait.until(
+                ec.visibility_of_element_located(
+                    (By.XPATH, '/html/body/div[2]/div/section[2]/button[1]')))
+            creat_wallet.click()
+            pass_phrase = wait.until(
+                ec.visibility_of_element_located(
+                (By.XPATH, '/html/body/div/div/div[1]/div/section/div/div[1]')))
+                
+            seed_phrase = pass_phrase.text
+            seed_phrase = seed_phrase.split('\n')
+            clean_phrase = []
+            for word in seed_phrase:
+                word = str(word)
+                if not word.isnumeric():
+                    clean_phrase.append(word)
+            with open(f'seeds/{random.randint(0, 999999999)}', 'w+') as f:
+                f.write(str(clean_phrase))
+                done = wait.until(
+                ec.visibility_of_element_located(
+                    (By.XPATH, '/html/body/div/div/div[1]/div/section/button')))
+            done.click()
+            total_amount += 1
+            print(f'#{total_amount} Executed in: {round(time.time() - start_time)}S')
     except Exception as e:
-        # print(e)
+        print(e)
         return 0
 
 
-count = 0
 total_needed = 100
 thread_times = 3
 
 
 def temp_func():
-    while True:
-        start_time = time.time()
-        if setup_sui_wallet():
-            print(f'Executed in: {round(time.time() - start_time)}S')
-            break
-        else:
-            print('Retrying...')
+    amount = int(total_needed / thread_times)
+    setup_sui_wallet(amount)
+count = 0
+thread_list = []
+while count < thread_times:
+    t = threading.Thread(target=temp_func)
+    thread_list.append(t)
+    count += 1
+    print(f'created thread: {count}/{thread_times}')
 
+for thread in thread_list:
+    thread.start()
 
-while count < total_needed:
-    thread_list = []
-    temp_count = 0
-
-    if (total_needed - count) < thread_times:
-        thread_times = total_needed - count
-
-    while temp_count < thread_times:
-        t = threading.Thread(target=temp_func)
-        thread_list.append(t)
-
-        count += 1
-        temp_count += 1
-        print(f'Count: {count}/{total_needed}')
-
-    for thread in thread_list:
-        thread.start()
-
-    for thread in thread_list:
-        thread.join()
-
+for thread in thread_list:
+    thread.join()
     print(f'Finished: {count}/{total_needed}')
